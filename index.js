9dcd5def144ffab9a38b68b8f5ee2aa1d63b3522
Andrew Peebles <aqpeeb@gmail.com>
2017-08-12 10:50:46 -0700

    initial checkin

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let lib = {};
+
+lib.exit = function( err ) {
+  if ( err ) console.error( err );
+  process.exit( err ? 1 : 0 );
+}
+
+lib.parseargs = function( argv ) {
+  let bag = {};
+  let arg;
+  
+  function cvt( v ) {
+    if ( ! v ) return v;
+    if ( v.match( /^\d+$/ ) ) return Number( v );
+    if ( v == 'true' ) return true;
+    if ( v == 'false' ) return false;
+    return v;
+  }
+  
+  while( arg = argv.shift() ) {
+    let i = arg.match( /^--(.+)/ );
+    if ( i && i.length == 2 ) {
+      if ( argv[0] && argv[0].match( /^--(.+)/ ) )
+        bag[ i[1] ] = true;
+      else if ( ! argv[0] )
+        bag[ i[1] ] = true;
+      else {
+        if ( bag[ i[1] ] ) {
+          if ( ! Array.isArray( bag[ i[1] ] ) ) { 
+            let e = bag[ i[1] ];
+            bag[ i[1] ] = [ cvt(e) ];
+          }
+          bag[ i[1] ].push( cvt( argv.shift() ) );
+        }
+        else {
+          bag[ i[1] ] = cvt( argv.shift() );
+        }
+      }
+    }
+  }
+  return bag;
+}
+
+module.exports = lib;
